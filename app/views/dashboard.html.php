93e02cc57f0ff0e5562fd6a46a336480171ae7f0
IbrahimaISIDev <sorydiallo371@gmail.com>
2024-04-21 12:24:49 +0000

    add multiple pages and fonctionalities

---
+++ app/views/dashboard.html.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="../public/css/home-promo.css">
+    <title>Sonatel Academy-Promo 6</title>
+</head>
+
+<body>
+    <div class="container">
+
+    </div>
+    <div class="bloc-promotion">
+        <p class="Num-1">1</p>
+        <p class="promo">Promotion</p>
+        <div class="barre">
+            <div class="form">
+                <form action="">
+                    <div class="form-group">
+                        <label for="">Libelle</label>
+                        <input type="text" placeholder="Enter le libelle*">
+                    </div>
+                    <div class="date">
+                        <div class="form-group">
+                            <label for="">Date Début</label>
+                            <input type="text" class="form-control" placeholder="MM/DD/YYYY*">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Date Fin</label>
+                            <input type="text" class="form-control" placeholder="MM/DD/YYYY*">
+                        </div>
+                    </div>
+                    <div class="btns">
+                        <div class="add">
+                            <a href="?name=referentiel">Ajouter Référentiel (s)</a>
+                        </div>
+                        <div class="create">
+                            <a href="?name=promotion">Créer Promotion</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <p class="Num-2">2</p>
+        <p class="ref">Référentiels</p>
+    </div>
+    </div>
+    </div>
+</body>
+
+</html>
